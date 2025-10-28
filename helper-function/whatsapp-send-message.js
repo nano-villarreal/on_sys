eee2600fca56fe55e7610191455fa2f6815d4d8e
Emiliano Villarreal <ev8559@utexas.edu>
2025-10-27 20:01:52 -0600

    first commit

--- helper-function/whatsapp-send-message.js
+++ helper-function/whatsapp-send-message.js
@@ -1,5 +1,5 @@
-var accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-var authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
+var accountSid = process.env.TWILIO_ACCOUNT_SID;
+var authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken, {
     lazyLoading: true
