a6ebcab76246ea2b224489973adacd5ab1772b25
Emiliano Villarreal <ev8559@utexas.edu>
2025-11-11 15:27:05 -0600

    render

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT;
 
 // Home route
 webApp.get('/', (req, res) => {
-    res.send(`Hello World.!`);
+    res.render('ui.ejs');
 });
 
 const WA = require('../helper-function/whatsapp-send-message');
